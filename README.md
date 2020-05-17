b8ada911a4d7dace3983fc4e22d88fd9a5ba6f59
BSoldi <briansoldani@gmail.com>
2020-05-16 21:08:18 -0700

    update readme and clean copy of js file for submission

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# 03-homework-assignment-03
-Unit 03 Javascript: Password Generator
+# Unit 03 Javascript: Password Generator
+
+## Description
+This assignment was to use the given HTML, CSS, and JavaScript files, with some starter code, to make a functioning random password generator. The final product needed to prompt users for input such as desired password length, whether to use numbers or symbols, and whether to use lowercase or uppercase letters. The password could be anywhere from 8 to 128 characters in length and only consist of the items chosen by the user through conifrming and prompts. HTML5, CSS3 and JavaScript were used and implemented for this project.
+
+
+### Project Details
+Using the above mentioned technologies and code sources, the deployed application was created. The application consists of the following:
+
+- A JavaScript function that generates a password.
+- Confirmation messages to the user for inclusion of types of letters, numbers, or symbols.
+- An alert if the user chooses none of these options.
+- A prompt asking the user for the desired password length.
+- An alert if the user chooses more or less than the given range of 8-128 characters.
+- There are four seperate functions that each contain references to sets of characters in an external chart, ubiquitous to coding. 
+- Based on the user selections and input, the password is generated for the desired length and contains the selected items.
+- Once the password displays on the page, the user is free to start the process over again.
+ 
+ #### Results
+ The final application performs as expected and meets the requirements given above. If the user selects the desired password elements to use, they are given a randomly generated password that meets their desired criteria. If they happen to not like the password given to them on the first attempt, they are able to run the process over and over again until a more desired password is achieved.
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- // Assignment Code
- var generateButton = document.querySelector("#generate");
+  // Assignment Code
+  var generateButton = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,10 +10,11 @@ function writePassword() {
 // // Add event listener to generate button
 generateButton.addEventListener('click', writePassword); 
 
-// This starts the alerts for user inputs for password contents
+// This starts the alerts asking for user inputs to be included in password
 
 function generatePassword () {
-  alert("Please choose options to include in your password. The password length should be between 8 and 128 characters long. Thank you!'");
+
+  alert("Please choose options to include in your password. The password length should be between 8 and 128 characters long. Thank you!");
 
   var isChoiceLower = confirm('Do you want Lower Case letters?');
   
@@ -23,12 +24,15 @@ function generatePassword () {
   
   var isChoiceSymbol = confirm('Do you want Symbols?');
   
-    // This confirms the user has inputs
+  
+  // This confirms the user has inputs or will reload the page if null
   
     if ((isChoiceLower === false) && (isChoiceUpper === false) && (isChoiceNumber === false) && (isChoiceSymbol === false)) {
       alert('You must choose at least 1 item to include. For better security, choose minimum of 3 items!');
       location.reload();
     }
+
+    // This confirms the user has input correct range for password length
 
     var userInput = prompt('How long would you like your password to be? (8-128 characters please)');
     var passwordLength = parseInt(userInput);
@@ -37,6 +41,9 @@ function generatePassword () {
       location.reload();
     } else if (passwordLength > 128) {
       alert('Password must have at less than 128 characters!');
+      location.reload();
+    } else if (passwordLength === null) {
+      alert('Password must have at least 8 characters!');
       location.reload();
     }
 
@@ -58,129 +65,35 @@ function getRandomSymbol() {
 }
 
 
+  // Get count of items that are true to inclue in for loop
 
-// Choices to include
-var funcArr = [getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol];
-
-  const lowerTrue = (isChoiceLower === true)
-  const upperTrue = (isChoiceUpper === true)
-  const numberTrue = (isChoiceNumber === true)
-  const symbolTrue = (isChoiceSymbol === true)
-
-let password = "";
-
-   const areTrue = lowerTrue + upperTrue + numberTrue + symbolTrue;
-
-   console.log("are true: ", areTrue);
-
-   const areTrueArr = [{lowerTrue}, {upperTrue}, {numberTrue}, {symbolTrue}].filter
-   (
-     item => Object.values(item)[0]
-   );
-
-   console.log("areTrueArr: ", areTrueArr);
+  const areTrue = (isChoiceLower === true) + (isChoiceUpper === true) + (isChoiceNumber === true) + (isChoiceSymbol === true);
 
 
-     // empty string variable for the for loop below
+  // Empty string variable for the for loop below
+
   var randomPasswordGenerated = "";
-  // loop getting random characters
+
+
+  // Loop getting random characters based on user selection, input, and length
+
   for (let i = 0; i < passwordLength; i++) {
-    var randomNumberPicked = Math.floor(Math.random() * 4);
-    functionArray=[
-      getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol
-    ]
+    var randomNumberPicked = Math.floor(Math.random() * areTrue);
+    var functionArray = []; 
+
+    if (isChoiceLower === true) {
+      functionArray.push(getRandomLower)
+    } 
+    if (isChoiceUpper === true) {
+        functionArray.push(getRandomUpper)
+    } 
+    if (isChoiceNumber === true) {
+        functionArray.push(getRandomNumber)
+    } 
+    if (isChoiceSymbol === true) {
+        functionArray.push(getRandomSymbol)
+    } 
     randomPasswordGenerated += functionArray[randomNumberPicked]();
-    console.log(randomPasswordGenerated)
   }
-
-      // randomPasswordGenerated += areTruelowerTrue;
-      // randomPasswordGenerated += areTrueupperTrue;
-      // randomPasswordGenerated += areTruenumberTrue;
-      // randomPasswordGenerated += areTruesymbolTrue;
-      
-      
-      return randomPasswordGenerated;
-
-
+  return randomPasswordGenerated;
 }
-
-
-
-      // for (var i = 0; i < passwordLength; i++) {
-        // areTrueArr.forEach(type => {
-        //   const funcName = Object.keys(type)[0];
-          
-        //   return String.password(gerneratePassword() += randomFunc[funcName]);
-
-
-//  // console.log(lowerTrue, upperTrue, numberTrue, symbolTrue)
-
-//   // const randomFunc = {
-//   //   lowerTrue: getRandomLower,
-//   //   upperTrue: getRandomUpper,
-//   //   numberTrue: getRandomNumber,
-//   //   symbolTrue: getRandomSymbol,  
-//   // };
-
-
-  
-  // var userInput = prompt('How long would you like your password to be? (8-128 characters please)');
-  // var passwordLength = parseInt(userInput);
-  // if (passwordLength < 8) {
-  //   alert('Password must have at least 8 characters!');
-  //   location.reload();
-  // } else if (passwordLength > 128) {
-  //   alert('Password must have at less than 128 characters!');
-  //   location.reload();
-  // } else 
-
-//   // empty string variable for the for loop below
-//   var randomPasswordGenerated = "";
-//   // loop getting random characters
-//   for (let i = 0; i < passwordLength - areTrue; i++) {
-//     var randomNumberPicked = Math.floor(Math.random() * 4);
-//     functionArray=[
-//       getRandomLower, getRandomUpper, getRandomNumber, getRandomSymbol
-//     ]
-//     // randomPasswordGenerated += functionArray[randomNumberPicked]();
-//     // console.log(randomPasswordGenerated)
-//   }
-//       // for (var i = 0; i < passwordLength; i++) {
-      //   areTrueArr.forEach(type => {
-      //     const funcName = Object.keys(type)[0];
-          
-          // return String.password(gerneratePassword() += randomFunc[funcName]);
-
-      
-        
-      // randomPasswordGenerated += areTruegetRandomLower;
-      // randomPasswordGenerated += areTruegetRandomUpper;
-      // randomPasswordGenerated += areTruegetRandomNumber;
-      // randomPasswordGenerated += areTruegetRandomSymbol;
-      
-      
-      // return randomPasswordGenerated;
-//}
-        
-
-    // function generatePassword () {
-  //   function getRandomFunc() {
-  //     return funcName[Math.floor(Math.random()*funcName.length)];
-  //  }
-        //  gerneratePassword += randomFunc[funcName]();
-        // console.log("Loop " + i);
-        // passwordGen = funcArr[Math.floor(Math.random() * funcArr.passwordLength) + i];}
-
-
-
-
-
-// console.log(isChoiceLower);
-// console.log(isChoiceUpper);
-// console.log(isChoiceNumber);
-// console.log(isChoiceSymbol);
-// console.log(passwordLength);
-// console.log(getRandomLower());
-// console.log(getRandomUpper());
-// console.log(getRandomNumber());
-// console.log(getRandomSymbol());
